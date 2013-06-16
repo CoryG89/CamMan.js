254d79326473cda475cf4a1ce45558f2461a9f33
CoryG89 <CoryG89@gmail.com>
2013-06-16 06:07:22 -0500

    Fixing typo in CamMan.js

--- CamMan.js
+++ CamMan.js
@@ -198,7 +198,6 @@ var CamMan = (function () {
         if (window.URL && window.URL.revokeObjectURL) {
             window.URL.revokeObjectURL(this.video.src);
         }
-        f
         this.canvasStorage.length = 0;
         return this.trigger('stop');
     };
